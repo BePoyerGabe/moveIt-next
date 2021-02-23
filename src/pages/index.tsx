b4c85cb98add90be8abc834b2b8c34012f33c337
BePoyerGabe <bernardopoyer00@gmail.com>
2021-02-23 12:47:32 -0300

    Countdows styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { CompletedChallenges } from '../components/CompleteChallenges';
+import { Countdown } from '../components/CountDown';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 
@@ -10,12 +11,16 @@ import styles from '../styles/pages/Home.module.css'
 export default function Home() {
   return (
     <div className={ styles.container }>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExperienceBar />
 
       <section>
         <div >
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
 
 
